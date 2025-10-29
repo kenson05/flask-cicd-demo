b453a267fa871d7673ee0034b8d22005ca563c9c
kenson05 <kenneth.son@wsu.edu>
2025-10-29 12:51:50 -0700

    fixed more linter errors

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1><p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
+    return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1>" \
+    "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-    
